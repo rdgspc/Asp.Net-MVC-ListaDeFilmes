d61bb57c23e1c73ebf95c3fa7b39f1549736c25e
Rodrigo <rdgcostacontatos@gmail.com>
2023-08-28 22:27:38 -0300

    Ajustes de Home - Falta imagem

---
+++ MVCFilmes/Migrations/20230829012444_Ajustes-Home.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCFilmes.Migrations
+{
+    public partial class AjustesHome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
